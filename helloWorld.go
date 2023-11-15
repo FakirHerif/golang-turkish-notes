3ce8192c5871e62edce950138b8c8fdd9b79e141
FakirHerif <basdemir35@hotmail.com.tr>
2023-11-15 23:13:53 +0300

    helloworld added

---
+++ helloWorld.go
@@ -0,0 +1,20 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World")
+}
+
+/*
+1- source kodumuzu compile etmek için:
+go build helloWorld.go
+yazıyoruz. Ardından "helloWorld.exe" adlı pcmizin okuyabileceği bir exe dosyası oluşturmuş olduk. terminalden "helloWorld" veya "helloWorld.exe" yazdığımız zaman "Hello, World" çıktımızı verecektir.
+
+2- Konsolda çıktıyı almak için:
+go run helloWorld.go
+yazıyoruz. ve konsolda "Hello, World" çıktısına ulaşıyoruz
+
+*/
